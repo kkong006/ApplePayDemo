6be1e456dee3d8529ac0230b23c7cde1760a9314
Karen Kong <kkong006@ucr.edu>
2018-03-31 01:05:55 -0700

    Initial Commit

---
+++ ApplePayDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApplePayDemo
+//
+//  Created by Karen Kong on 3/31/18.
+//  Copyright © 2018 Karen Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
